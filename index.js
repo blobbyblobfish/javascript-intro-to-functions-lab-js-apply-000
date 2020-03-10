afee07f5e1d98c3c586bab5b3d6276f51d0bb674
Blobby Blobfish <aywang6@gmail.com>
2020-03-10 17:48:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,16 +7,21 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  console.log() string.toUpperCase
+  console.log(); string.toUpperCase
 }
 
 function logWhisper(string) {
-  var whisperString = string.toLowerCase
-  console.log(whisperString)
+    console.log(); string.toLowerCase
 }
 
-function sayHiToGrandma(String) {
-  if 
-  
-}
+function sayHiToGrandma(string) {
+  if ((string)===uppercase) {
+    return 'YES INDEED!'
+  }
+  else if ((string)===lowercase) {
+    return "I can't hear you!"
+  }
+  else if ((string)==="I love you, Grandma.")) {
+    return "I love you, too."
+  }
 }
